bd93a4a9ffa02504655cae64877ea1daa0062277
robblofield <141638136+robblofield@users.noreply.github.com>
2024-02-15 17:04:14 +0000

    Lab folder tidied up for student download

--- Assets/Scripts/BowType.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class BowType : MonoBehaviour
-{
-    public string typeName; // Name of the bow type
-    public float damageMultiplier; // Damage multiplier for the bow
-    public float pullbackTime; // Pullback time for the bow
-}
